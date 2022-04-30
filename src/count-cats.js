46a6b09413816c01b0a1139c4303ccc305bd8bb4
Jenyajk <101403726+Jenyajk@users.noreply.github.com>
2022-04-30 12:42:11 +0500

    fix: compete countCats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,10 +14,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(array) {
+return array.flat().filter((el) => el==='^^').length;
 }
+
+
+
 
 module.exports = {
   countCats
